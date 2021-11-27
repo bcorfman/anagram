a4b645c3e259a03caa1f10a6fb6eb7d386960970
Brandon Corfman <bcorfman@fastmail.fm>
2021-11-26 23:55:18 -0500

    FEATURE: perf optimization with initial bag for first word

--- anagram.py
+++ anagram.py
@@ -11,14 +11,19 @@ def anagrams(search_word: str, word_list: list) -> list:
     candidate_word_list = [word for word in word_list if bag(word).issubset(letters_in_search_word)]
     solutions = []
     for i, first_word in enumerate(candidate_word_list):
+        first_bag = bag(first_word)
         for j in range(i+1, len(candidate_word_list)):
             second_word = candidate_word_list[j]
-            if bag(first_word + second_word) == letters_in_search_word:
+            if first_bag + bag(second_word) == letters_in_search_word:
                 solutions.append(f'{first_word} {second_word}')
                 solutions.append(f'{second_word} {first_word}')
     return solutions
 
 
-if __name__ == '__main__':
+def main():
     for pairs in anagrams('documenting', read_word_list()):
         print(pairs)
+
+
+if __name__ == '__main__':
+    main()
